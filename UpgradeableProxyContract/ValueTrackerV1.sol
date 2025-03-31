cd650359d7e248d1d90d37e47c1a2fae9d34746e
CodeByAnkita <ankitacode11@gmail.com>
2025-03-31 13:42:36 +0700

    added valuetrackerv1

--- UpgradeableProxyContract/ValueTrackerV1.sol
+++ UpgradeableProxyContract/ValueTrackerV1.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract ValueTrackerV2 is Initializable, UUPSUpgradeable, OwnableUpgradeable {
+contract ValueTrackerV1 is Initializable, UUPSUpgradeable, OwnableUpgradeable {
     uint256 public trackedValue;
     uint256 public incrementCount; // New feature in V2
 
